16daef5c639daa3d6399326ba19feb2af2f831fa
Rani Hoegnason Hansen <rani.hoegnason@gmail.com>
2019-02-01 14:18:50 +0000

    added travis-ci config file

--- src/core/collision-detection.ts
+++ src/core/collision-detection.ts
@@ -11,6 +11,7 @@ export class CollisionDection {
 
     // Axis-Aligned Bounding Box
     // https://developer.mozilla.org/en-US/docs/Games/Techniques/2D_collision_detection
+    /*
     public rectOnRect(rect1: IRect, rect2: IRect): boolean {
         if (rect1.x < rect2.x + rect2.width &&
             rect1.x + rect1.width > rect2.x &&
@@ -24,4 +25,19 @@ export class CollisionDection {
             return false;
         }
     }
+    */
+
+   public rectOnRect(rect1: IRect, rect2: IRect): boolean {
+    if (rect1.x < rect2.x + rect2.width &&
+        rect1.x + rect1.width > rect2.x &&
+        rect1.y < rect2.y + rect2.height &&
+        rect1.y + rect1.height > rect2.y) {
+
+        return true;
+
+    } else {
+
+        return false;
+    }
+}
 }
--- src/core/physics-engine.ts
+++ src/core/physics-engine.ts
@@ -2,32 +2,57 @@ import { CollisionDection, IRect } from './collision-detection';
 
 export interface IBody extends IRect {
     dynamic: boolean;
-    velocity: [number, number];
+    velocity: IVector;
     colided: boolean;
 };
+
+export interface IPoint {
+    x: number;
+    y: number;
+}
+
+export interface IVector {
+    x: number;
+    y: number;
+}
+
+export interface IPhysicsEngineOptions {
+    gravity: IVector;
+}
+
+export interface IBodyCollision {
+    bodyA: IBody;
+    bodyB: IBody;
+}
 
 export class PhysicsEngine {
 
     private collisionDection: CollisionDection;
+    private gravity: IVector;
 
-    constructor() {
+    constructor(options?: IPhysicsEngineOptions) {
         this.collisionDection = new CollisionDection();
+        this.gravity = {x: 0, y: 1};
     }
 
-
-    public tick(bodies: IBody[], options: any[]) {     
+    public tick(bodies: IBody[]) {     
         const staticBodies = bodies.filter((body: IBody) => !body.dynamic);
         const dynamicBodies = bodies.filter((body: IBody) => body.dynamic);
 
-        this.applyGravity(bodies, 1);
+        this.applyGravity(dynamicBodies);
         this.checkCollisions(staticBodies, dynamicBodies);
     }
 
-    private applyGravity(dynamicBodies: IBody[], gravity: number) {
-        /* */
+    private applyGravity(dynamicBodies: IBody[]) {
+        dynamicBodies.forEach((body: IBody) => {
+            body.velocity.x = body.velocity.x + this.gravity.x;
+            body.velocity.y = body.velocity.y + this.gravity.y;
+        });
     }
 
-    private checkCollisions(staticBodies: IBody[], dynamicBodies: IBody[]) {
+    private checkCollisions(staticBodies: IBody[], dynamicBodies: IBody[]): IBodyCollision[] {
+        const collisions: IBodyCollision[] = [];
+
         if (null != dynamicBodies && null != staticBodies) {
 
             staticBodies.forEach((staticBody: IBody) => {
@@ -36,10 +61,12 @@ export class PhysicsEngine {
                     const colided = this.collisionDection.rectOnRect(staticBody, dynamicBody);
 
                     if (colided) {
-                        /* */
+                        collisions.push({bodyA: dynamicBody, bodyB: staticBody});
                     }
                 });
             });
         }
+
+        return collisions;
     }
 }
