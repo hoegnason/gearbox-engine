810f66803603791fb77a7a1ebe612c80347c86a6
Hans Kari <hanskari132@gmail.com>
2019-02-18 22:07:53 +0000

    blackbox

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,26 +1,29 @@
 import * as React from "react";
 
 export default class Menu extends React.Component<any> {
-  constructor(props: any) {
-    super(props);
-  }
 
   public render() {
     const menuStyle: React.CSSProperties = {
-      alignContent: "center",
-      backgroundColor: "#949494"
+      backgroundColor: "black",
+      width: '100%'
+    }
+    const newGame = () => {
+        return (
+            <div>New Game</div>
+        )
     }
 
     const showMenu = () => {
       return (
-        <div style={{width: `${100 * this.context.scale}px`, height: `${100 * this.context.scale}px`, zIndex: 9999999, backgroundColor: 'black'}} >
+        <div style={{align: 'center', width: `${100 * this.context.scale}px`, height: `${100 * this.context.scale}px`, zIndex: 9999, backgroundColor: 'black'}} > 
           <ul>
-            <li>newgame</li>
+            <li>{newGame()}</li>
+            <li>Highscore</li>
           </ul>
         </div>
       )
     }
 
-    return <div style={menuStyle}>{showMenu}</div>
+    return <div style={menuStyle}>{showMenu()}</div>
   }
 }
