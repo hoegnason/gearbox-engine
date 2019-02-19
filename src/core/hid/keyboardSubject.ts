9ed4536e9952e7b9b25355c05a33632a5b4f1a8d
Rani Hoegnason Hansen <rani.hoegnason@gmail.com>
2019-02-19 14:41:02 +0000

    Cleaned up code

--- src/core/hid/keyboardSubject.ts
+++ src/core/hid/keyboardSubject.ts
@@ -1,6 +1,11 @@
-import { fromEvent } from 'rxjs';
+import { fromEvent, merge } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+const SPACE = 32;
+
+const touchObs = fromEvent(document, 'touchstart').pipe(map((event: TouchEvent) => SPACE));
+
 export const keyboardObs =
-    fromEvent(document, 'keydown')
-        .pipe(map((event: KeyboardEvent) => (null != event && null != event.which) ? event.which : null ));
+    merge(
+        fromEvent(document, 'keydown').pipe(map((event: KeyboardEvent) => (null != event && null != event.which) ? event.which : null )),
+        touchObs);
