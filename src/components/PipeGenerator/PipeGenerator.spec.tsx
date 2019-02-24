90ceaa26f083717a82687f687db90d762d747c2c
essybern <esberngeilini@gmail.com>
2019-02-24 15:55:42 +0000

    Unit test to Pipe Generator

--- src/components/PipeGenerator/PipeGenerator.spec.tsx
+++ src/components/PipeGenerator/PipeGenerator.spec.tsx
@@ -9,7 +9,7 @@ describe('PipeGenerator functionality', async () => {
 
     it("Should Generate array of pipes", async () => {
 
-        const wrapper = shallow(<PipeGenerator gameState={{ x: 0 }} />);
+        const wrapper = shallow(<PipeGenerator gameState={{ x: -1000 }} />);
 
         expect(wrapper.find('div').length).toEqual(1)
 
