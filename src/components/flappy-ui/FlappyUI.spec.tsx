8fef9d8fb26d7629a4350b1f9c57b8455b586f2b
Rani Hoegnason Hansen <rani.hoegnason@gmail.com>
2019-03-01 03:07:01 +0000

    Added a Component to declerativly scale text

--- src/components/flappy-ui/FlappyUI.spec.tsx
+++ src/components/flappy-ui/FlappyUI.spec.tsx
@@ -9,9 +9,33 @@ const context = { scale: 1, width: 1920, height: 1080 };
 describe('FlappyUI', async () => {
 
 
-    it("should have no divs", async () => {
+    it('should have no divs', async () => {
 
         const gameState = {...defaultProps};
+        gameState.ready = true;
+
+        const wrapper = mount(<FlappyUI gameState={gameState} />, { context });
+
+        // One div for the wrapper div and one div for readyElement and one for scoreElement
+        expect(wrapper.find('div').length).toBe(2);
+    });
+
+    it('should show gameOver element', () => {
+        
+        const gameState = {...defaultProps};
+        gameState.gameOver = true;
+        
+        const wrapper = mount(<FlappyUI gameState={gameState} />, { context })
+
+        // One div for the wrapper div and one div for readyElement and one for scoreElement
+        expect(wrapper.find('div').length).toBe(4);
+    });
+
+    it('should show ready element', () => {
+        
+        const gameState = {...defaultProps};
+        gameState.ready = false;
+        
         const wrapper = mount(<FlappyUI gameState={gameState} />, { context })
 
         // One div for the wrapper div and one div for readyElement and one for scoreElement
